bce685bad7fd4e5bf7a49f6816c435748386c68f
Keonity <56704285+Keonity@users.noreply.github.com>
2024-08-24 02:24:14 -0700

    Initial commit with fibs function per assignment.

--- src/index.js
+++ src/index.js
@@ -1,44 +1,57 @@
 import './styles.css';
-import testBg from './coffeeBg.jpg';
 
-console.log(`Welcome To The Eye Of Odin Weather Project`);
+console.log(`Welcome To The Odin Recursion Project`);
 
-const resultHeader = document.querySelector(`#resultHeader`);
-const description = document.querySelector(`#description`);
-const searchbar = document.querySelector(`#searchbar`);
-const submit = document.querySelector(`#submit`);
-const loadAnimation = document.querySelector(`#loadAnimationOff`);
+function fibs(n) {
+    let fibNum = 0 // Sequence begins at 0
+    const fibArray =  new Array(n);
+    // fibArray[0] = fibNum;
 
-const queryLocation = `Las Vegas`;
-
-async function getWeather(data = `Las Vegas`) {
-    try {
-        loadAnimation.setAttribute(`id`, `loadAnimation`);
-        let response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${data}?unitGroup=metric&key=5MGVRN4YAV8UJY9MGZB77KHLE&contentType=json`);
-        if (!response.ok) {
-            throw new Error(response.status)
+    for (let i = 0; i < n; i++) {
+        if (i === 0) {
+            // Do nothing
         }
-        let weatherData = await response.json();
-        loadAnimation.setAttribute(`id`, `loadAnimationOff`)
-        console.log(weatherData.currentConditions.temp);
-        resultHeader.innerHTML = `The temperature in ${weatherData.resolvedAddress} is ${weatherData.currentConditions.temp}` + `&#8451`;
-        description.innerHTML = `${weatherData.description}`
-    }
-    catch (error) {
-        if (error.message === `400`) {
-            loadAnimation.setAttribute(`id`, `loadAnimationOff`);
-            resultHeader.innerHTML = `Error ${error.message}: Invalid search parameters, please try again with a different location.`;
+        else if (i === 1 || i === 2) {
+            fibNum = 1;
         }
         else {
-            loadAnimation.setAttribute(`id`, `loadAnimationOff`);
-            resultHeader.innerHTML = `Error ${error.message}`;
+            fibNum = (fibArray[i - 2] + fibArray[i - 1]);
         }
-        description.innerHTML = ``;
+        fibArray[i] = fibNum;
+        console.log(i, fibArray);
     }
+    return fibArray;
 }
 
-submit.addEventListener("click", () => {
-    getWeather(searchbar.value);
-});
+function fibsRec(n) {
+    if (n <= 0) {
+        return;
+    }
+    if (n === 1) {
+        const fibArray = new Array(1);
+        fibArray[0] = 0;
+        return fibArray;
+    }
+    else {
+        const fibArray = new Array(n);
+    }
+    return;
+}
 
-getWeather();
+function facsRec(n) {
+    // const fibArray = new Array(n);
+    if (n === 1) {
+        // fibsArray[i] = 1;
+        return 1;
+    }
+    else {
+        // fibsArray
+        return n * facsRec(n - 1);
+    }
+};
+
+console.log(facsRec(5));
+
+fibs(8);
+
+// console.log(fibs(8));
